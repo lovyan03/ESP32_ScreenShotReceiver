c076e11fa9694041947b33ca5d86ffc5ffae224a
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-07-18 10:43:55 +0900

    add: TCPReceiverDual.h

---
+++ ScreenShotReceiver/src/TCPReceiverDual.h
@@ -0,0 +1,380 @@
+#ifndef _TCPRECEIVER_H_
+#define _TCPRECEIVER_H_
+
+#pragma GCC optimize ("O3")
+
+#ifndef LOVYANGFX_HPP_
+#include <LovyanGFX.hpp>  // https://github.com/lovyan03/LovyanGFX/
+#endif
+
+#include <WiFi.h>
+#include <WiFiServer.h>
+#include <esp_heap_caps.h>
+#include "tjpgdClass.h"
+
+#undef min
+
+class TCPReceiver
+{
+public:
+  bool setup(LovyanGFX* lcd0, LovyanGFX* lcd1)
+  {
+    _lcd[0] = lcd0;
+    _lcd[1] = lcd1;
+    Serial.println("TCPReceiver setup.");
+
+    _lcd_width = lcd0->width();
+    _lcd_height = lcd0->height();
+    _bytesize = lcd0->getColorConverter()->bytes;
+    _fp_jpgWrite = (_bytesize == 2)
+                 ? jpgWrite16
+                 : jpgWrite24
+                 ;
+    for (int i = 0; i < 2; ++i) {
+      for (int j = 0; j < 2; ++j) {
+        _dmabufs[i][j] = (uint8_t*)heap_caps_malloc(_lcd_width * 48 * _bytesize, MALLOC_CAP_DMA);
+        Serial.printf("buf:%08x \n", (uint32_t)_dmabufs[i][j]);
+      }
+      _dmabuf[i] = _dmabufs[i][0];
+      _lcd[i]->startWrite();
+      _lcd[i]->initDMA();
+      delay(10);
+    }
+    _tcp.setNoDelay(true);
+    _tcp.begin(63333);
+
+    _jdec.multitask_begin();
+
+
+    Serial.println("TCPReceiver setup done.");
+    return true;
+  }
+
+  bool loop()
+  {
+    static uint32_t waitCount = 0;
+
+    if (SENDER_PREFIX_SIZE <= _client.available()
+     && SENDER_PREFIX_SIZE == _client.read(_tcpBuf, SENDER_PREFIX_SIZE)) {
+      waitCount = 0;
+      if (_tcpBuf[0] == 'J'
+       && _tcpBuf[1] == 'P'
+       && _tcpBuf[2] == 'G') {
+        _recv_remain = _tcpBuf[3] | _tcpBuf[4] << 8 | _tcpBuf[5] << 16 | _tcpBuf[6] << 24;
+        if (_recv_remain > 600) {
+          if (drawJpg()) {
+            ++_drawCount;
+          }
+        } else {
+          Serial.println("jpg too short");
+        }
+        if (0 < _recv_remain) {
+          Serial.printf("clear remain data:%d\r\n", _recv_remain);
+          int r;
+          for (uint32_t retry = 1000; retry; --retry) {
+            r = _client.read(_tcpBuf, _recv_remain < TCP_BUF_LEN ? _recv_remain : TCP_BUF_LEN);
+            if (r > 0) {
+              _recv_remain -= r;
+              if (!_recv_remain) break;
+            } else {
+              delay(1);
+              ++_delayCount;
+            }
+          }
+        }
+      } else {
+        Serial.println("broken data");
+        delay(10);
+        while (0 < _client.read(_tcpBuf, TCP_BUF_LEN)) delay(10);
+      }
+    } else
+    if (!_client.connected()) {
+      _client = _tcp.available();
+      _recv_requested = false;
+      waitCount = 0;
+    } else {
+      if (++waitCount > 1000) {
+        _recv_requested = false;
+      } else {
+        delay(1);
+        ++_delayCount;
+      }
+      if (!_recv_requested)   {
+        while (0 < _client.read(_tcpBuf, TCP_BUF_LEN));
+        _recv_requested = true;
+        waitCount = 0;
+        Serial.println("data request");
+        _client.print("JPG\n");
+      }
+    }
+
+    if (_sec != millis() / 1000) {
+      _sec = millis() / 1000;
+      Serial.printf("%2d fps", _drawCount);
+      if (_delayCount) Serial.printf(" / delay%3d", _delayCount);
+      Serial.print("\r\n");
+      _drawCount = 0;
+      _delayCount = 0;
+    }
+
+    return true;
+  }
+private:
+  static constexpr int TCP_BUF_LEN = 512;
+  static constexpr int SENDER_PREFIX_SIZE = 7;
+  static constexpr uint32_t _rowskip = 2;
+
+  LovyanGFX* _lcd[2];
+  uint_fast8_t _bytesize;
+  uint8_t* _dmabufs[2][2];
+  uint8_t* _dmabuf[2];
+
+  WiFiServer _tcp;
+  WiFiClient _client;
+  TJpgD _jdec;
+  int32_t _recv_remain = 0;
+  uint32_t _sec = 0;
+  int32_t _lcd_width;
+  int32_t _lcd_height;
+  int32_t _out_width;
+  int32_t _out_height;
+  int32_t _off_x;
+  int32_t _off_y;
+  int32_t _jpg_x;
+  int32_t _jpg_y;
+  uint32_t _drawCount = 0;
+  uint32_t _delayCount = 0;
+  uint8_t _tcpBuf[TCP_BUF_LEN];
+  bool _recv_requested = false;
+
+  uint32_t(*_fp_jpgWrite)(TJpgD*,void*,TJpgD::JRECT*);
+
+  static uint32_t jpgRead(TJpgD *jdec, uint8_t *buf, uint32_t len) {
+    TCPReceiver* me = (TCPReceiver*)jdec->device;
+    WiFiClient* client = &me->_client;
+    uint32_t retry;
+
+    if (len == TJPGD_SZBUF) {
+      if (me->_recv_requested) {
+        me->_recv_requested = false;
+      } else
+      if (me->_recv_remain < TJPGD_SZBUF*2 && TJPGD_SZBUF < me->_recv_remain) { // dataend read tweak
+        len = me->_recv_remain - len;
+      }
+    } else if (client->available() < len) {
+      for (retry = 1000; client->available() < len && retry; --retry) {
+        delay(1);
+        ++me->_delayCount;
+      }
+    }
+
+    int l = client->read(buf ? buf : me->_tcpBuf, len);
+    if (l <= 0) {
+      for (retry = 1000; retry; --retry) {
+        delay(1);
+        ++me->_delayCount;
+        l = client->read(buf ? buf : me->_tcpBuf, len);
+        if (l > 0) break;
+      }
+    }
+    if (l <= 0) {
+      Serial.printf("jpgRead error:%d:%d:%d\r\n", l, len, client->available());
+      return 0;
+    }
+    me->_recv_remain -= l;
+    if (!me->_recv_requested && me->_recv_remain < 3) {
+      if (me->_recv_remain >= client->available()) {
+        client->print("JPG\n"); // request the next image from the client
+        me->_recv_requested = true;
+      } else {
+        Serial.println("excessive request");
+        me->_recv_requested = true;
+      }
+    }
+    return l;
+  }
+
+  static uint32_t jpgWrite24(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
+    TCPReceiver* me = (TCPReceiver*)jdec->device;
+
+    auto dst = me->_dmabuf[0];
+
+    uint_fast16_t outWidth = me->_out_width;
+    uint_fast16_t outHeight = me->_out_height;
+    if (rect->left >= outWidth) {
+      rect->left -= outWidth;
+      rect->right -= outWidth;
+      dst = me->_dmabuf[1];
+    }
+    uint_fast16_t x = rect->left;
+    uint_fast16_t y = rect->top;
+    uint_fast16_t w = rect->right + 1 - x;
+    uint_fast16_t h = rect->bottom + 1 - y;
+    uint8_t *src = (uint8_t*)bitmap;
+    uint_fast16_t oL = 0, oR = 0;
+
+    if (rect->right < me->_off_x)      return 1;
+    if (x >= (me->_off_x + outWidth))  return 1;
+    if (rect->bottom < me->_off_y)     return 1;
+    if (y >= (me->_off_y + outHeight)) return 1;
+
+    int32_t src_idx = 0;
+    int32_t dst_idx = 0;
+
+    uint_fast16_t tmpy = y % ((1 + _rowskip) * jdec->msy << 3);
+    if (me->_off_y > y) {
+      uint_fast16_t linesToSkip = me->_off_y - y;
+      src_idx += linesToSkip * w;
+      h -= linesToSkip;
+      dst_idx -= tmpy * outWidth;
+    } else 
+    if (me->_off_y > (y - tmpy)) {
+      uint_fast16_t linesToSkip = me->_off_y - (y - tmpy);
+      dst_idx -= linesToSkip * outWidth;
+    }
+
+    if (me->_off_x > x) {
+      oL = me->_off_x - x;
+    }
+    if (rect->right >= (me->_off_x + outWidth)) {
+      oR = (rect->right + 1) - (me->_off_x + outWidth);
+    }
+
+    int_fast16_t line = (w - ( oL + oR )) * 3;
+    dst_idx += oL + x - me->_off_x + outWidth * tmpy;
+    src_idx += oL;
+    do {
+      memcpy(&dst[dst_idx * 3], &src[src_idx * 3], line);
+      dst_idx += outWidth;
+      src_idx += w;
+    } while (--h);
+
+    return 1;
+  }
+
+  static uint32_t jpgWrite16(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
+    TCPReceiver* me = (TCPReceiver*)jdec->device;
+
+    uint16_t *dst = (uint16_t *)me->_dmabuf[0];
+
+    uint_fast16_t outWidth = me->_out_width;
+    uint_fast16_t outHeight = me->_out_height;
+    if (rect->left >= outWidth) {
+      rect->left -= outWidth;
+      rect->right -= outWidth;
+      dst = (uint16_t *)me->_dmabuf[1];
+    }
+    uint_fast16_t x = rect->left;
+    uint_fast16_t y = rect->top;
+    uint_fast16_t w = rect->right + 1 - x;
+    uint_fast16_t h = rect->bottom + 1 - y;
+    uint8_t *src = (uint8_t*)bitmap;
+    uint_fast16_t oL = 0, oR = 0;
+
+    if (rect->right < me->_off_x)      return 1;
+    if (x >= (me->_off_x + outWidth))  return 1;
+    if (rect->bottom < me->_off_y)     return 1;
+    if (y >= (me->_off_y + outHeight)) return 1;
+
+    uint_fast16_t tmpy = y % ((1 + _rowskip) * jdec->msy << 3);
+    if (me->_off_y > y) {
+      uint_fast16_t linesToSkip = me->_off_y - y;
+      src += linesToSkip * w * 3;
+      h -= linesToSkip;
+      dst -= tmpy * outWidth;
+    } else 
+    if (me->_off_y > (y - tmpy)) {
+      uint_fast16_t linesToSkip = me->_off_y - (y - tmpy);
+      dst -= linesToSkip * outWidth;
+    }
+
+    if (me->_off_x > x) {
+      oL = me->_off_x - x;
+    }
+    if (rect->right >= (me->_off_x + outWidth)) {
+      oR = (rect->right + 1) - (me->_off_x + outWidth);
+    }
+
+    int_fast16_t line = (w - ( oL + oR ));
+    dst += oL + x - me->_off_x + outWidth * tmpy;
+    src += oL * 3;
+    do {
+      int i = 0;
+      do {
+        uint_fast8_t r8 = src[i*3+0] & 0xF8;
+        uint_fast8_t g8 = src[i*3+1];
+        uint_fast8_t b5 = src[i*3+2] >> 3;
+        r8 |= g8 >> 5;
+        g8 &= 0x1C;
+        b5 = (g8 << 3) + b5;
+        dst[i] = r8 | b5 << 8;
+      } while (++i != line);
+      dst += outWidth;
+      src += w * 3;
+    } while (--h);
+
+    return 1;
+  }
+
+  static uint32_t jpgWriteRow(TJpgD *jdec, uint32_t y, uint32_t h) {
+    static int flip = 0;
+    TCPReceiver* me = (TCPReceiver*)jdec->device;
+
+    if (y == 0) {
+      me->_lcd[0]->setAddrWindow(me->_jpg_x, me->_jpg_y, me->_out_width, me->_out_height);
+      me->_lcd[1]->setAddrWindow(me->_jpg_x, me->_jpg_y, me->_out_width, me->_out_height);
+    }
+    int16_t outY = y - me->_off_y;
+    if (outY < 0) {
+      if (h <= - outY) return 1;
+      h += outY;
+      outY = 0;
+    }
+    if (me->_lcd_height <= me->_jpg_y + outY) return 1;
+    if (me->_lcd_height < me->_jpg_y + outY + h) {
+      h = me->_lcd_height - (me->_jpg_y + outY);
+    }
+
+    me->_lcd[ flip]->pushPixelsDMA(me->_dmabuf[ flip], me->_out_width * h);
+    me->_lcd[!flip]->pushPixelsDMA(me->_dmabuf[!flip], me->_out_width * h);
+
+    flip = !flip;
+    me->_dmabuf[0] = me->_dmabufs[0][flip];
+    me->_dmabuf[1] = me->_dmabufs[1][flip];
+    return 1;
+  }
+
+  bool drawJpg() {
+    TJpgD::JRESULT jres = _jdec.prepare(jpgRead, this);
+    if (jres != TJpgD::JDR_OK) {
+      Serial.printf("prepare failed! %d\r\n", jres);
+      return false;
+    }
+    _out_width = std::min<int32_t>(_jdec.width>>1, _lcd_width);
+    _jpg_x = (_lcd_width - (_jdec.width>>1)) >> 1;
+    if (0 > _jpg_x) {
+      _off_x = - _jpg_x;
+      _jpg_x = 0;
+    } else {
+      _off_x = 0;
+    }
+    _out_height = std::min<int32_t>(_jdec.height, _lcd_height);
+    _jpg_y = (_lcd_height- _jdec.height) >> 1;
+    if (0 > _jpg_y) {
+      _off_y = - _jpg_y;
+      _jpg_y = 0;
+    } else {
+      _off_y = 0;
+    }
+
+    jres = _jdec.decomp_multitask(_fp_jpgWrite, jpgWriteRow, _rowskip);
+    if (jres != TJpgD::JDR_OK) {
+      Serial.printf("decomp failed! %d\r\n", jres);
+      return false;
+    }
+
+    return true;
+  }
+};
+
+#endif
--- ScreenShotSender/FormSenderMain.Designer.cs
+++ ScreenShotSender/FormSenderMain.Designer.cs
@@ -62,8 +62,9 @@
             // 
             this.btnShowFrame.Dock = System.Windows.Forms.DockStyle.Left;
             this.btnShowFrame.Location = new System.Drawing.Point(0, 0);
+            this.btnShowFrame.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnShowFrame.Name = "btnShowFrame";
-            this.btnShowFrame.Size = new System.Drawing.Size(81, 19);
+            this.btnShowFrame.Size = new System.Drawing.Size(135, 28);
             this.btnShowFrame.TabIndex = 0;
             this.btnShowFrame.Text = "ShowBox";
             this.btnShowFrame.UseVisualStyleBackColor = true;
@@ -72,9 +73,10 @@
             // btnStartStopr
             // 
             this.btnStartStopr.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnStartStopr.Location = new System.Drawing.Point(81, 0);
+            this.btnStartStopr.Location = new System.Drawing.Point(135, 0);
+            this.btnStartStopr.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnStartStopr.Name = "btnStartStopr";
-            this.btnStartStopr.Size = new System.Drawing.Size(81, 19);
+            this.btnStartStopr.Size = new System.Drawing.Size(135, 28);
             this.btnStartStopr.TabIndex = 1;
             this.btnStartStopr.Text = "Connect";
             this.btnStartStopr.UseVisualStyleBackColor = true;
@@ -90,15 +92,15 @@
             this.pnlTop1.Controls.Add(this.btnShowFrame);
             this.pnlTop1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTop1.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.pnlTop1.Name = "pnlTop1";
-            this.pnlTop1.Size = new System.Drawing.Size(334, 19);
+            this.pnlTop1.Size = new System.Drawing.Size(557, 28);
             this.pnlTop1.TabIndex = 2;
             // 
             // nudRow
             // 
             this.nudRow.Dock = System.Windows.Forms.DockStyle.Left;
-            this.nudRow.Location = new System.Drawing.Point(246, 0);
-            this.nudRow.Margin = new System.Windows.Forms.Padding(2);
+            this.nudRow.Location = new System.Drawing.Point(403, 0);
             this.nudRow.Maximum = new decimal(new int[] {
             4,
             0,
@@ -110,7 +112,7 @@
             0,
             0});
             this.nudRow.Name = "nudRow";
-            this.nudRow.Size = new System.Drawing.Size(41, 19);
+            this.nudRow.Size = new System.Drawing.Size(68, 25);
             this.nudRow.TabIndex = 4;
             this.nudRow.Value = new decimal(new int[] {
             1,
@@ -123,10 +125,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label3.Location = new System.Drawing.Point(223, 0);
-            this.label3.MinimumSize = new System.Drawing.Size(0, 19);
+            this.label3.Location = new System.Drawing.Point(368, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.MinimumSize = new System.Drawing.Size(0, 28);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(23, 19);
+            this.label3.Size = new System.Drawing.Size(35, 28);
             this.label3.TabIndex = 7;
             this.label3.Text = "row";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -134,8 +137,7 @@
             // nudCol
             // 
             this.nudCol.Dock = System.Windows.Forms.DockStyle.Left;
-            this.nudCol.Location = new System.Drawing.Point(182, 0);
-            this.nudCol.Margin = new System.Windows.Forms.Padding(2);
+            this.nudCol.Location = new System.Drawing.Point(300, 0);
             this.nudCol.Maximum = new decimal(new int[] {
             4,
             0,
@@ -147,7 +149,7 @@
             0,
             0});
             this.nudCol.Name = "nudCol";
-            this.nudCol.Size = new System.Drawing.Size(41, 19);
+            this.nudCol.Size = new System.Drawing.Size(68, 25);
             this.nudCol.TabIndex = 3;
             this.nudCol.Value = new decimal(new int[] {
             1,
@@ -160,10 +162,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label1.Location = new System.Drawing.Point(162, 0);
-            this.label1.MinimumSize = new System.Drawing.Size(0, 19);
+            this.label1.Location = new System.Drawing.Point(270, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(0, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 19);
+            this.label1.Size = new System.Drawing.Size(30, 28);
             this.label1.TabIndex = 6;
             this.label1.Text = "col";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -171,10 +174,10 @@
             // nudQuality
             // 
             this.nudQuality.Dock = System.Windows.Forms.DockStyle.Left;
-            this.nudQuality.Location = new System.Drawing.Point(195, 0);
-            this.nudQuality.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.nudQuality.Location = new System.Drawing.Point(307, 0);
+            this.nudQuality.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.nudQuality.Name = "nudQuality";
-            this.nudQuality.Size = new System.Drawing.Size(43, 19);
+            this.nudQuality.Size = new System.Drawing.Size(72, 25);
             this.nudQuality.TabIndex = 14;
             this.nudQuality.Value = new decimal(new int[] {
             60,
@@ -187,10 +190,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label2.Location = new System.Drawing.Point(154, 0);
-            this.label2.MinimumSize = new System.Drawing.Size(0, 19);
+            this.label2.Location = new System.Drawing.Point(245, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.MinimumSize = new System.Drawing.Size(0, 28);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 19);
+            this.label2.Size = new System.Drawing.Size(62, 28);
             this.label2.TabIndex = 13;
             this.label2.Text = "Quality";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -198,8 +202,8 @@
             // nudHeight
             // 
             this.nudHeight.Dock = System.Windows.Forms.DockStyle.Left;
-            this.nudHeight.Location = new System.Drawing.Point(111, 0);
-            this.nudHeight.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.nudHeight.Location = new System.Drawing.Point(173, 0);
+            this.nudHeight.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.nudHeight.Maximum = new decimal(new int[] {
             320,
             0,
@@ -211,7 +215,7 @@
             0,
             0});
             this.nudHeight.Name = "nudHeight";
-            this.nudHeight.Size = new System.Drawing.Size(43, 19);
+            this.nudHeight.Size = new System.Drawing.Size(72, 25);
             this.nudHeight.TabIndex = 4;
             this.nudHeight.Value = new decimal(new int[] {
             120,
@@ -222,10 +226,10 @@
             // nudWidth
             // 
             this.nudWidth.Dock = System.Windows.Forms.DockStyle.Left;
-            this.nudWidth.Location = new System.Drawing.Point(32, 0);
-            this.nudWidth.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.nudWidth.Location = new System.Drawing.Point(48, 0);
+            this.nudWidth.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.nudWidth.Maximum = new decimal(new int[] {
-            480,
+            640,
             0,
             0,
             0});
@@ -235,7 +239,7 @@
             0,
             0});
             this.nudWidth.Name = "nudWidth";
-            this.nudWidth.Size = new System.Drawing.Size(43, 19);
+            this.nudWidth.Size = new System.Drawing.Size(72, 25);
             this.nudWidth.TabIndex = 3;
             this.nudWidth.Value = new decimal(new int[] {
             160,
@@ -248,8 +252,9 @@
             this.pbPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pbPreview.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pbPreview.Location = new System.Drawing.Point(0, 0);
+            this.pbPreview.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.pbPreview.Name = "pbPreview";
-            this.pbPreview.Size = new System.Drawing.Size(334, 243);
+            this.pbPreview.Size = new System.Drawing.Size(557, 365);
             this.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbPreview.TabIndex = 3;
             this.pbPreview.TabStop = false;
@@ -263,9 +268,10 @@
             // 
             this.pnlAll.Controls.Add(this.pbPreview);
             this.pnlAll.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlAll.Location = new System.Drawing.Point(0, 58);
+            this.pnlAll.Location = new System.Drawing.Point(0, 87);
+            this.pnlAll.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.pnlAll.Name = "pnlAll";
-            this.pnlAll.Size = new System.Drawing.Size(334, 243);
+            this.pnlAll.Size = new System.Drawing.Size(557, 365);
             this.pnlAll.TabIndex = 4;
             // 
             // pnlTop2
@@ -277,19 +283,21 @@
             this.pnlTop2.Controls.Add(this.nudWidth);
             this.pnlTop2.Controls.Add(this.lblWidth);
             this.pnlTop2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTop2.Location = new System.Drawing.Point(0, 41);
+            this.pnlTop2.Location = new System.Drawing.Point(0, 61);
+            this.pnlTop2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.pnlTop2.Name = "pnlTop2";
-            this.pnlTop2.Size = new System.Drawing.Size(334, 17);
+            this.pnlTop2.Size = new System.Drawing.Size(557, 26);
             this.pnlTop2.TabIndex = 5;
             // 
             // lblHeight
             // 
             this.lblHeight.AutoSize = true;
             this.lblHeight.Dock = System.Windows.Forms.DockStyle.Left;
-            this.lblHeight.Location = new System.Drawing.Point(75, 0);
-            this.lblHeight.MinimumSize = new System.Drawing.Size(0, 19);
+            this.lblHeight.Location = new System.Drawing.Point(120, 0);
+            this.lblHeight.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblHeight.MinimumSize = new System.Drawing.Size(0, 28);
             this.lblHeight.Name = "lblHeight";
-            this.lblHeight.Size = new System.Drawing.Size(36, 19);
+            this.lblHeight.Size = new System.Drawing.Size(53, 28);
             this.lblHeight.TabIndex = 6;
             this.lblHeight.Text = "height";
             this.lblHeight.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -299,9 +307,10 @@
             this.lblWidth.AutoSize = true;
             this.lblWidth.Dock = System.Windows.Forms.DockStyle.Left;
             this.lblWidth.Location = new System.Drawing.Point(0, 0);
-            this.lblWidth.MinimumSize = new System.Drawing.Size(0, 19);
+            this.lblWidth.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblWidth.MinimumSize = new System.Drawing.Size(0, 28);
             this.lblWidth.Name = "lblWidth";
-            this.lblWidth.Size = new System.Drawing.Size(32, 19);
+            this.lblWidth.Size = new System.Drawing.Size(48, 28);
             this.lblWidth.TabIndex = 5;
             this.lblWidth.Text = "width";
             this.lblWidth.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -312,23 +321,24 @@
             this.tlpHost.ColumnCount = 1;
             this.tlpHost.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlpHost.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tlpHost.Location = new System.Drawing.Point(0, 19);
+            this.tlpHost.Location = new System.Drawing.Point(0, 28);
             this.tlpHost.Margin = new System.Windows.Forms.Padding(0);
             this.tlpHost.Name = "tlpHost";
             this.tlpHost.RowCount = 1;
-            this.tlpHost.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
-            this.tlpHost.Size = new System.Drawing.Size(334, 22);
+            this.tlpHost.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tlpHost.Size = new System.Drawing.Size(557, 33);
             this.tlpHost.TabIndex = 3;
             // 
             // FormSenderMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 301);
+            this.ClientSize = new System.Drawing.Size(557, 452);
             this.Controls.Add(this.pnlAll);
             this.Controls.Add(this.pnlTop2);
             this.Controls.Add(this.tlpHost);
             this.Controls.Add(this.pnlTop1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "FormSenderMain";
             this.Text = "ScreenShotSender";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormSenderMain_FormClosed);
