70f712c3e58be8165f240116d8efc817b939920e
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-01-13 23:49:41 +0900

    tweak for grayscale mode

--- ScreenShotReceiver/src/TCPReceiver.h
+++ ScreenShotReceiver/src/TCPReceiver.h
@@ -28,17 +28,22 @@ public:
     _bytesize = lcd->getColorConverter()->bytes;
     Serial.printf("display byte per pixel:%d\n", _bytesize);
 
-    switch (_bytesize)
+    Serial.printf("color depth: %02x \n", lcd->getColorDepth());
+
+    switch (lcd->getColorDepth())
     {
     default:
-    case 2:
-      _fp_jpgWrite = jpgWrite16;
+    case lgfx::color_depth_t::rgb565_2Byte:
+      _fp_write = write565;
       break;
-    case 3:
-      _fp_jpgWrite = jpgWrite24;
+    case lgfx::color_depth_t::rgb888_3Byte:
+      _fp_write = write888;
       break;
-    case 1:
-      _fp_jpgWrite = jpgWrite8;
+    case lgfx::color_depth_t::rgb332_1Byte:
+      _fp_write = write332;
+      break;
+    case lgfx::color_depth_t::grayscale_8bit:
+      _fp_write = writeGray;
       break;
     }
     for (int i = 0; i < 2; ++i) { _dmabufs[i] = (uint8_t*)heap_caps_malloc(_lcd_width * (_rowskip + 1) * 16 * _bytesize, MALLOC_CAP_DMA); }
@@ -88,7 +93,7 @@ public:
           }
         }
       } else {
-        Serial.printf("broken data: %02x %02x %02x %02x %02x %02x %02x \n", _tcpBuf[0], _tcpBuf[1], _tcpBuf[2], _tcpBuf[3], _tcpBuf[4], _tcpBuf[5], _tcpBuf[6], _tcpBuf[7]);
+        Serial.printf("broken data: %02x %02x %02x %02x %02x %02x %02x %02x \n", _tcpBuf[0], _tcpBuf[1], _tcpBuf[2], _tcpBuf[3], _tcpBuf[4], _tcpBuf[5], _tcpBuf[6], _tcpBuf[7]);
         do { delay(8); } while (0 < _client.read(_tcpBuf, TCP_BUF_LEN));
       }
     } else
@@ -190,7 +195,7 @@ private:
     return l;
   }
 
-  static uint32_t jpgWrite24(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
+  static uint32_t jpgWrite(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
     TCPReceiver* me = (TCPReceiver*)jdec->device;
 
     auto dst = me->_dmabuf;
@@ -231,131 +236,95 @@ private:
       oR = (rect->right + 1) - (me->_off_x + outWidth);
     }
 
-    int_fast16_t line = (w - ( oL + oR )) * 3;
+    int_fast16_t line = (w - ( oL + oR ));
     dst_idx += oL + x - me->_off_x + outWidth * tmpy;
     src_idx += oL;
-    do {
-      memcpy(&dst[dst_idx * 3], &src[src_idx * 3], line);
-      dst_idx += outWidth;
-      src_idx += w;
-    } while (--h);
+
+    me->_fp_write(&dst[dst_idx * me->_bytesize], &src[src_idx * 3], line, outWidth, w, h);
 
     return 1;
   }
 
-  static uint32_t jpgWrite16(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
-    TCPReceiver* me = (TCPReceiver*)jdec->device;
-
-    uint16_t *dst = (uint16_t *)me->_dmabuf;
-
-    uint_fast16_t x = rect->left;
-    uint_fast16_t y = rect->top;
-    uint_fast16_t w = rect->right + 1 - x;
-    uint_fast16_t h = rect->bottom + 1 - y;
-    uint_fast16_t outWidth = me->_out_width;
-    uint_fast16_t outHeight = me->_out_height;
-    uint8_t *src = (uint8_t*)bitmap;
-    uint_fast16_t oL = 0, oR = 0;
-
-    if (rect->right < me->_off_x)      return 1;
-    if (x >= (me->_off_x + outWidth))  return 1;
-    if (rect->bottom < me->_off_y)     return 1;
-    if (y >= (me->_off_y + outHeight)) return 1;
-
-    uint_fast16_t tmpy = y % ((1 + _rowskip) * jdec->msy << 3);
-    if (me->_off_y > y) {
-      uint_fast16_t linesToSkip = me->_off_y - y;
-      src += linesToSkip * w * 3;
-      h -= linesToSkip;
-      dst -= tmpy * outWidth;
-    } else 
-    if (me->_off_y > (y - tmpy)) {
-      uint_fast16_t linesToSkip = me->_off_y - (y - tmpy);
-      dst -= linesToSkip * outWidth;
-    }
-
-    if (me->_off_x > x) {
-      oL = me->_off_x - x;
-    }
-    if (rect->right >= (me->_off_x + outWidth)) {
-      oR = (rect->right + 1) - (me->_off_x + outWidth);
-    }
-
-    int_fast16_t line = (w - ( oL + oR ));
-    dst += oL + x - me->_off_x + outWidth * tmpy;
-    src += oL * 3;
+  static void write888(uint8_t* dst, const uint8_t* src, uint32_t line, uint32_t outWidth, uint32_t w, uint32_t h)
+  {
+    line *= 3;
+    outWidth *= 3;
+    w *= 3;
     do {
-      int i = 0;
-      do {
-        uint_fast8_t r8 = src[i*3+0] & 0xF8;
-        uint_fast8_t g8 = src[i*3+1];
-        uint_fast8_t b5 = src[i*3+2] >> 3;
-        r8 |= g8 >> 5;
-        g8 &= 0x1C;
-        b5 = (g8 << 3) + b5;
-        dst[i] = r8 | b5 << 8;
-      } while (++i != line);
+      memcpy(dst, src, line);
+      src += w;
       dst += outWidth;
-      src += w * 3;
     } while (--h);
-
-    return 1;
   }
 
-  static uint32_t jpgWrite8(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
-    TCPReceiver* me = (TCPReceiver*)jdec->device;
-
-    uint8_t *dst = (uint8_t *)me->_dmabuf;
-
-    uint_fast16_t x = rect->left;
-    uint_fast16_t y = rect->top;
-    uint_fast16_t w = rect->right + 1 - x;
-    uint_fast16_t h = rect->bottom + 1 - y;
-    uint_fast16_t outWidth = me->_out_width;
-    uint_fast16_t outHeight = me->_out_height;
-    uint8_t *src = (uint8_t*)bitmap;
-    uint_fast16_t oL = 0, oR = 0;
-
-    if (rect->right < me->_off_x)      return 1;
-    if (x >= (me->_off_x + outWidth))  return 1;
-    if (rect->bottom < me->_off_y)     return 1;
-    if (y >= (me->_off_y + outHeight)) return 1;
-
-    uint_fast16_t tmpy = y % ((1 + _rowskip) * jdec->msy << 3);
-    if (me->_off_y > y) {
-      uint_fast16_t linesToSkip = me->_off_y - y;
-      src += linesToSkip * w * 3;
-      h -= linesToSkip;
-      dst -= tmpy * outWidth;
-    } else 
-    if (me->_off_y > (y - tmpy)) {
-      uint_fast16_t linesToSkip = me->_off_y - (y - tmpy);
-      dst -= linesToSkip * outWidth;
-    }
-
-    if (me->_off_x > x) {
-      oL = me->_off_x - x;
-    }
-    if (rect->right >= (me->_off_x + outWidth)) {
-      oR = (rect->right + 1) - (me->_off_x + outWidth);
-    }
-
-    int_fast16_t line = (w - ( oL + oR ));
-    dst += oL + x - me->_off_x + outWidth * tmpy;
-    src += oL * 3;
+  static void write565(uint8_t* dst, const uint8_t* src, uint32_t line, uint32_t outWidth, uint32_t w, uint32_t h)
+  {
     do {
-      int i = 0;
-      do {
-        uint_fast8_t r8 = src[i*3+0] & 0xE0;
-        uint_fast8_t g5 = (src[i*3+1] >> 3) & 0x1C;
-        uint_fast8_t b2 = src[i*3+2] >> 6;
-        dst[i] = r8 | g5 | b2;
-      } while (++i != line);
+      auto s = src;
+      auto d = dst;
+      uint32_t l = line;
+      while (l--) {
+        uint32_t r = s[0];
+        uint32_t g = s[1];
+        uint32_t b = s[2];
+        s += 3;
+        g = g >> 2;
+        r = (r >> 3) & 0x1F;
+        r = (r << 3) + (g >> 3);
+        b = (b >> 3) + (g << 5);
+        d[0] = r;
+        d[1] = b;
+        d += 2;
+      }
+      dst += outWidth * 2;
+      src += w * 3;
+    } while (--h);
+  }
+
+  static void write332(uint8_t* dst, const uint8_t* src, uint32_t line, uint32_t outWidth, uint32_t w, uint32_t h)
+  {
+    do {
+      auto s = src;
+      auto d = dst;
+      uint32_t l = line;
+      while (l--) {
+        uint32_t r = s[0];
+        uint32_t g = s[1];
+        uint32_t b = s[2];
+        s += 3;
+        r >>= 5;
+        g >>= 5;
+        b >>= 6;
+        g += r << 3;
+        b += g << 2;
+        d[0] = b;
+        ++d;
+      }
       dst += outWidth;
       src += w * 3;
     } while (--h);
+  }
 
-    return 1;
+  static void writeGray(uint8_t* dst, const uint8_t* src, uint32_t line, uint32_t outWidth, uint32_t w, uint32_t h)
+  {
+    --dst;
+    do {
+      auto s = src;
+      auto d = dst;
+      uint32_t l = line;
+      while (l--) {
+        uint32_t r = s[0];
+        uint32_t g = s[1];
+        uint32_t b = s[2];
+        s += 3;
+        ++d;
+        r += (g << 1);
+        r += b;
+        d[0] = r >> 2;
+      }
+      dst += outWidth;
+      src += w * 3;
+    } while (--h);
   }
 
   static uint32_t jpgWriteRow(TJpgD *jdec, uint32_t y, uint32_t h) {
@@ -364,6 +333,8 @@ private:
 
     if (y == 0) {
       me->_lcd->setAddrWindow(me->_jpg_x, me->_jpg_y, me->_out_width, me->_out_height);
+
+      // while (me->_lcd->getScanLine() > 64) { taskYIELD(); }
     }
 
     int16_t outY = y - me->_off_y;
@@ -377,12 +348,13 @@ private:
       h = me->_lcd_height - (me->_jpg_y + outY);
     }
 
-    switch (me->_lcd->getColorConverter()->bytes)
+    switch (me->_lcd->getColorDepth())
     {
     default:
-    case 2:  me->_lcd->pushPixelsDMA((lgfx::swap565_t*)(me->_dmabuf), me->_out_width * h);  break;
-    case 1:  me->_lcd->pushPixelsDMA((lgfx::rgb332_t*)(me->_dmabuf), me->_out_width * h);  break;
-    case 3:  me->_lcd->pushPixelsDMA((lgfx::bgr888_t*)(me->_dmabuf), me->_out_width * h);  break;
+    case lgfx::color_depth_t::rgb565_2Byte:   me->_lcd->pushPixelsDMA((lgfx::swap565_t*  )(me->_dmabuf), me->_out_width * h);  break;
+    case lgfx::color_depth_t::rgb888_3Byte:   me->_lcd->pushPixelsDMA((lgfx::bgr888_t*   )(me->_dmabuf), me->_out_width * h);  break;
+    case lgfx::color_depth_t::rgb332_1Byte:   me->_lcd->pushPixelsDMA((lgfx::rgb332_t*   )(me->_dmabuf), me->_out_width * h);  break;
+    case lgfx::color_depth_t::grayscale_8bit: me->_lcd->pushPixelsDMA((lgfx::grayscale_t*)(me->_dmabuf), me->_out_width * h);  break;
     }
 
     flip = !flip;
@@ -413,7 +385,7 @@ private:
       _off_y = 0;
     }
 
-    jres = _jdec.decomp_multitask(_fp_jpgWrite, jpgWriteRow, _rowskip);
+    jres = _jdec.decomp_multitask(jpgWrite, jpgWriteRow, _rowskip);
     if (jres != TJpgD::JDR_OK) {
       Serial.printf("decomp failed! %d\r\n", jres);
       return false;
@@ -422,7 +394,7 @@ private:
     return true;
   }
 
-  uint32_t(*_fp_jpgWrite)(TJpgD*,void*,TJpgD::JRECT*) = jpgWrite16;
+  void (*_fp_write)(uint8_t* dst, const uint8_t* src, uint32_t line, uint32_t outWidth, uint32_t w, uint32_t h);
 };
 
 #endif
